26991393350e0c9424e5677e98e3f7dd21ba6bc0
Ryan Semmler <semmra7@gmail.com>
2017-03-02 17:52:07 -0500

    latest update for movie_lib.py

--- movie_lib.py
+++ movie_lib.py
@@ -20,24 +20,34 @@ rtg_by_movie_id = {}
 rtg_by_user_id = {}
 movie_names = {}
 
-with open('u.data') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        row = row[0].split('\t')
-        row[2] = int(row[2])
-        if row[1] in rtg_by_movie_id:
-            rtg_by_movie_id[row[1]].append(row[2])
-        else:
-            rtg_by_movie_id[row[1]] = [row[2]]
-        if row[0] in rtg_by_user_id:
-            rtg_by_user_id[row[0]].append(row[2])
-        else:
-            rtg_by_user_id[row[0]] = [row[2]]
 
-with open('u.item', encoding='latin_1') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        row = row[0].split('|')
-        movie_names[row[0]] = row[1]
+def get_movie_data():
+    with open('u.data') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            row = row[0].split('\t')
+            row[2] = int(row[2])
+            if row[1] in rtg_by_movie_id:
+                rtg_by_movie_id[row[1]].append(row[2])
+            else:
+                rtg_by_movie_id[row[1]] = [row[2]]
+            if row[0] in rtg_by_user_id:
+                rtg_by_user_id[row[0]].append((row[1], row[2]))
+            else:
+                rtg_by_user_id[row[0]] = [(row[1], row[2])]
 
-print(movie_names)
+    with open('u.item', encoding='latin_1') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            row = row[0].split('|')
+            movie_names[row[0]] = row[1]
+
+    return(rtg_by_movie_id, rtg_by_user_id, movie_names)
+
+
+def popular():
+    popular = [rating for rating in rtg_by_movie_id if len(rtg_by_movie_id[rating][1]) > 5]
+    return popular
+
+
+print(popular())
